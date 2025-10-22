d2f03e7cca52b534506caac08f60a4695316139b
hafizlayman <m.hafizlayman@gmail.com>
2025-10-22 21:07:46 +0800

    Initial commit - BuckDuit Flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, 'templates'),
+    static_folder=os.path.join(BASE_DIR, 'static')
+)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
